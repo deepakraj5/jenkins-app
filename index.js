0d4b57ce9d65746592919e6c1b11c8ae195871c0
root <root@ip-172-31-39-209.ap-south-1.compute.internal>
2021-07-22 14:15:19 +0000

    app running

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+
+const PORT = process.env.PORT || 3000
+
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+
+	res.send('app running')
+
+})
+
+
+
+app.listen(PORT, console.log(`server upon port ${PORT}`))
